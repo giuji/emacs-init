ade73df2bad6bd8bb28f02e74a42ecffa0184554
giuji <64275280+giuji@users.noreply.github.com>
2024-08-05 00:04:32 +0200

    disabled visual bell

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(setq visible-bell 'ignore)
+(setq ring-bell-function 'ignore)
 (setq make-pointer-invisible t)
 
 ;; Disable toolkit dialogs
